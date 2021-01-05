28093e482243b38d8674e8349bb27e632986efd1
parthmehra <49879965+parthmehra@users.noreply.github.com>
2021-01-05 12:57:50 +0530

    Add files via upload

--- CalenderDemo/src/main/java/com/example/parth/CalenderWork.java
+++ CalenderDemo/src/main/java/com/example/parth/CalenderWork.java
@@ -1,6 +1,7 @@
 package com.example.parth;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class CalenderWork {
     LocalDate d = LocalDate.now();
@@ -8,5 +9,9 @@ public class CalenderWork {
     public void ChangeDate(int n){
 
         System.out.println(d.plusDays(n));
+        System.out.println(DateTimeFormatter.ofPattern("dd-MM-yyyy").format(d.plusDays(n)));
+        System.out.println(DateTimeFormatter.ofPattern("dd-MMM-yyyy").format(d.plusDays(n)));
+        System.out.println(DateTimeFormatter.ofPattern("dd-MMMM-yy").format(d.plusDays(n)));
+        System.out.println(DateTimeFormatter.ofPattern("dd-MMMM-yyyy").format(d.plusDays(n)));
     }
 }
--- CalenderDemo/src/main/java/com/example/parth/Main.java
+++ CalenderDemo/src/main/java/com/example/parth/Main.java
@@ -3,7 +3,7 @@ package com.example.parth;
 public class Main {
     public static void main(String[] args) {
         CalenderWork cal=new CalenderWork();
-        cal.ChangeDate(-2);
+        cal.ChangeDate(2);
     }
 
 
